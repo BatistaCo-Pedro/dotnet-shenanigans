21f6f3316b3d60b146c807604c5788830fd09d6d
Batista Pedro <pedrocorreiabatista@gmail.com>
2024-08-21 22:13:18 +0200

    shenanigans

---
+++ Expressions/Basics.cs
@@ -0,0 +1,95 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ExpressionTrees;
+
+public class Basics
+{
+    internal Action<int>? updateCapturedLocalVariable;
+    internal Predicate<int> isEqualToCapturedLocalVariable;
+    
+    public void AllTypes()
+    {
+        Action<bool> action = Console.WriteLine;
+        
+        Func<int, bool> predicateFunc = num => num < 5;
+        
+        Predicate<int> predicate = num => num < 5;
+    
+        Expression<Func<int, bool>> expressionPredicate = num => num < 5;
+    
+        Expression<Func<int, bool>> predicateAsExpression = num => predicate(num);
+
+        LambdaExpression inferredLambdaExpression = (int num) => num < 5; //   Func<int, bool>
+        
+        Expression inferredExpression = (int num) => num < 5; //   Func<int, bool>
+        
+        var inferredType = (int num) => num < 5; //   Func<int, bool>
+
+        var specificReturnType = bool (int num) => num < 5;
+
+        var objectReturnType = object (int num) => num < 5 ? "String" : 1L;
+        
+        PredicateDelegate predicateDelegate = num => num < 5;
+        
+        var increment = [return: NotNullIfNotNull(nameof(s))] (int? s) => s.HasValue ? s++ : null;
+    }
+
+    public void ParamsWithinLambdaExpressions(params int[] nums)
+    {
+        var sum = (params int[] numbers) => numbers.Sum();
+        
+        var result = sum(nums);
+        
+        Console.WriteLine($"Sum result: {result}");
+    }
+
+    public async Task AsyncLambdaExpression()
+    {
+        AsyncDelegate asyncDelegate = async someStuff =>
+        {
+            Console.WriteLine("This is before waiting one second.");
+            await Task.Delay(1000);
+            Console.WriteLine(someStuff);
+        };
+
+        await asyncDelegate("Hi there, im printing some stuff after waiting one second.");
+    }
+
+    public void GenericDelegates()
+    {
+        GenericDelegate<int, bool> genericDelegate = num => num < 5;
+    }
+
+    public void PreventVariableCapture(int num)
+    {
+        Func<int, bool> staticLambda = static num => num < 5;
+
+        var result = staticLambda(num);
+        
+        Console.WriteLine($"Input number of {num} is bigger than 5: {result}");
+    }
+
+    public void VariableCapture(int input)
+    {
+        var i = 0;
+
+        updateCapturedLocalVariable = x =>
+        {
+            i = x;
+            var result = i > input;
+            Console.WriteLine($"{i} is greater than {input}: {result}");
+        };
+
+        isEqualToCapturedLocalVariable = x => x == i;
+
+        Console.WriteLine($"Local variable before lambda invocation: {i}");
+        updateCapturedLocalVariable(10);
+        Console.WriteLine($"Local variable after lambda invocation: {i}");
+    }
+
+    private delegate bool PredicateDelegate(int num);
+    
+    private delegate Task AsyncDelegate(string someStuff);
+    
+    private delegate TResult GenericDelegate<in TInput, out TResult>(TInput input);
+}
---
+++ Expressions/ExpressionTrees.cs
@@ -0,0 +1,125 @@
+﻿namespace ExpressionTrees;
+
+public class BuildingExpressionTrees
+{
+    public LambdaExpression BuildConstant()
+    {
+        return Expression.Lambda(Expression.Constant(1, typeof(int)));
+    }
+
+    public LambdaExpression BuildBinaryConstant()
+    {
+        var left = Expression.Constant(1, typeof(int));
+        var right = Expression.Constant(2, typeof(int));
+
+        return Expression.Lambda(Expression.Add(left, right));
+    }
+
+    private LambdaExpression BuildBinaryConstantOneStatement()
+    {
+        return Expression.Lambda(
+            Expression.Add(Expression.Constant(1, typeof(int)), Expression.Constant(2, typeof(int)))
+        );
+    }
+    
+    public LambdaExpression BuildParameter()
+    {
+        var parameter = Expression.Parameter(typeof(int), "num");
+
+        return Expression.Lambda(parameter);
+    }
+
+    // Distance calc
+    public LambdaExpression BuildMultipleParameters()
+    {
+        var xParameter = Expression.Parameter(typeof(double), "x");
+        var yParameter = Expression.Parameter(typeof(double), "y");
+        
+        var xSquared = Expression.Multiply(xParameter, xParameter);
+        var ySquared = Expression.Multiply(yParameter, yParameter);
+        
+        var sum = Expression.Add(xSquared, ySquared);
+        var sqrtMethod = typeof(Math).GetMethod(nameof(Math.Sqrt), [typeof(double)]) ?? throw new InvalidOperationException("Couldn't get sqrt method");
+
+        var distance = Expression.Call(sqrtMethod, sum);
+        
+        return Expression.Lambda(distance, xParameter, yParameter);
+    }
+
+    public BlockExpression CodeBlock()
+    {
+        /*
+            Func<int, int> factorialFunc = (n) =>
+            {
+                var res = 1;
+                while (n > 1)
+                {
+                    res = res * n;
+                    n--;
+                }
+                return res;
+            };
+         */
+
+        var label = Expression.Label(typeof(int));
+        
+        var n = Expression.Parameter(typeof(int), "n");
+
+        var result = Expression.Variable(typeof(int), "result");
+        var initResult = Expression.Assign(result, Expression.Constant(1, typeof(int)));
+
+        var multiplication = Expression.Multiply(result, n);
+
+        // Inner block of the while loop
+        var block = Expression.Block(Expression.Assign(result, multiplication), Expression.Decrement(result));
+
+        var loop = Expression.Loop(Expression.IfThenElse(Expression.GreaterThan(result, Expression.Constant(1, typeof(int))), block, Expression.Break(label, result)), label);
+
+        var expressionBody = Expression.Block(result, initResult, loop);
+
+        return expressionBody;
+    }
+
+    public Expression<Func<int, bool>> MapCode()
+    {
+        // Manually build the expression tree for
+        // the lambda expression num => num < 5.
+
+        var numParam = Expression.Parameter(typeof(int), "num");
+        var constantFive = Expression.Constant(5, typeof(int));
+
+        return Expression.Lambda<Func<int, bool>>(Expression.LessThan(numParam, constantFive), numParam);
+    }
+}
+
+public class TranslateExpression
+{
+    public static Expression ReplaceNode(Expression original)
+    {
+        if (original.NodeType == ExpressionType.Constant)
+        {
+            return Expression.Multiply(original, Expression.Constant(10, typeof(int)));
+        }
+
+        if (original.NodeType == ExpressionType.Add)
+        {
+            var binaryExpression = (BinaryExpression)original;
+            return Expression.Add(ReplaceNode(binaryExpression.Left), ReplaceNode(binaryExpression.Right));
+        }
+
+        return original;
+    }
+
+    public static void RunExpression()
+    {
+        var one = Expression.Constant(1, typeof(int));
+        var two = Expression.Constant(2, typeof(int));
+        var addition = Expression.Add(one, two);
+        var sum = ReplaceNode(addition);
+        var executableFunc = Expression.Lambda(sum);
+
+        var func = (Func<int>)executableFunc.Compile();
+        var answer = func();
+        Console.WriteLine(answer);
+    }
+}
