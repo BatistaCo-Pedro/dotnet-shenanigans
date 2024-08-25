e16408c285315fc130018faa2c3e7a5afcdfeb46
Batista Pedro <pedrocorreiabatista@gmail.com>
2024-08-25 22:23:37 +0200

    added patterns

---
+++ Patterns/Proxy.cs
@@ -0,0 +1,65 @@
+﻿namespace Patterns.Proxy;
+
+public interface ISubject
+{
+    void Request();
+}
+
+public class RealSubject : ISubject
+{
+    public void Request()
+    {
+        Console.WriteLine("RealSubject.Request()");
+    }
+}
+
+class Proxy(RealSubject realSubject) : ISubject
+{
+    public void Request()
+    {
+        if (CheckAccess())
+        {
+            realSubject.Request();
+
+            LogAccess();
+        }
+    }
+    
+    public bool CheckAccess()
+    {
+        Console.WriteLine("Proxy: Checking access prior to firing a real request.");
+
+        return true;
+    }
+    
+    public void LogAccess()
+    {
+        Console.WriteLine("Proxy: Logging the time of request.");
+    }
+}
+
+public class Client
+{
+    public void ClientCode(ISubject subject)
+    {
+        subject.Request();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var client = new Client();
+        var realSubject = new RealSubject();
+        var proxy = new Proxy(realSubject);
+        
+        Console.WriteLine("Client: Executing the client code with a real subject:");
+        client.ClientCode(realSubject);
+        
+        Console.WriteLine();
+        
+        Console.WriteLine("Client: Executing the same client code with a proxy:");
+        client.ClientCode(proxy);
+    }
+}
---
+++ Patterns/VisitorPattern.cs
@@ -0,0 +1,99 @@
+﻿namespace Patterns.Visitor;
+
+public interface IVisitor
+{
+    void VisitConcreteComponentA(ConcreteComponentA element);
+
+    void VisitConcreteComponentB(ConcreteComponentB element);
+}
+
+public interface IComponent
+{
+    void Accept(IVisitor visitor);
+}
+
+public class ConcreteComponentA : IComponent
+{
+    public void Accept(IVisitor visitor)
+    {
+        visitor.VisitConcreteComponentA(this);
+    }
+    
+    public string OperationOfA()
+    {
+        return "ConcreteComponentA.Operation()";
+    }
+}
+
+public class ConcreteComponentB : IComponent
+{
+    public void Accept(IVisitor visitor)
+    {
+        visitor.VisitConcreteComponentB(this);
+    }
+    
+    public string OperationOfB()
+    {
+        return "ConcreteComponentB.Operation()";
+    }
+}
+
+public class ConcreteVisitor1 : IVisitor
+{
+    public void VisitConcreteComponentA(ConcreteComponentA element)
+    {
+        Console.WriteLine(element.OperationOfA() + " + ConcreteVisitor1");
+    }
+
+    public void VisitConcreteComponentB(ConcreteComponentB element)
+    {
+        Console.WriteLine(element.OperationOfB() + " + ConcreteVisitor1");
+    }
+}
+
+public class ConcreteVisitor2 : IVisitor
+{
+    public void VisitConcreteComponentA(ConcreteComponentA element)
+    {
+        Console.WriteLine(element.OperationOfA() + " + ConcreteVisitor2");
+    }
+
+    public void VisitConcreteComponentB(ConcreteComponentB element)
+    {
+        Console.WriteLine(element.OperationOfB() + " + ConcreteVisitor2");
+    }
+}
+
+public class Client
+{
+    public static void ClientCode(List<IComponent> components, IVisitor visitor)
+    {
+        foreach (var component in components)
+        {
+            component.Accept(visitor);
+        }
+    }
+}
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<IComponent> components = new List<IComponent>
+        {
+            new ConcreteComponentA(),
+            new ConcreteComponentB()
+        };
+
+        Console.WriteLine("The client code works with all visitors via the base Visitor interface:");
+        var visitor1 = new ConcreteVisitor1();
+        Client.ClientCode(components,visitor1);
+
+        Console.WriteLine();
+
+        Console.WriteLine("It allows the same client code to work with different types of visitors:");
+        var visitor2 = new ConcreteVisitor2();
+        Client.ClientCode(components, visitor2);
+    }
+}
