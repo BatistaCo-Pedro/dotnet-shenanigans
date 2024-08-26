8d4179574f7ad691668033f22c9e01a8e6e574cb
Pedro <Pedrocorreiabatista@gmail.com>
2024-08-26 16:35:07 +0200

    Add solid principles project

---
+++ SOLID/OpenClosedPrinciple.cs
@@ -0,0 +1,34 @@
+﻿namespace SOLID;
+
+public class Rectangle
+{
+    public double Height { get; set; }
+    public double Width { get; set; }
+}
+
+public class Circle
+{
+    public double Radius { get; set; }
+}
+
+public class AreaCalculator
+{
+    public double TotalArea(object[] arrObjects)
+    {
+        double area = 0;
+        Circle objCircle;
+        foreach (var obj in arrObjects)
+        {
+            if (obj is Rectangle objRectangle)
+            {
+                area += objRectangle.Height * objRectangle.Width;
+            }
+            else
+            {
+                objCircle = (Circle)obj;
+                area += objCircle.Radius * objCircle.Radius * Math.PI;
+            }
+        }
+        return area;
+    }
+}
---
+++ SOLID/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,72 @@
+﻿namespace SOLID;
+
+public class ClassWithTooMuchResponsibility
+{
+    public void ConnectToDatabase(string connectionString)
+    {
+        // logic to connect to database
+    }
+
+    public void SetupDatabase(string options)
+    {
+        // logic to setup database
+    }
+
+    public void DisconnectFromDatabase()
+    {
+        // logic to disconnect from database
+    }
+
+    public void CreateUser()
+    {
+        // logic to create user
+    }
+
+    public void CreateRole()
+    {
+        // logic to create role
+    }
+    
+    public void AssignRoleToUser()
+    {
+        // logic to assign role to user
+    }
+}
+
+// -- Better approach --
+
+public class DatabaseManager
+{
+    public void ConnectToDatabase(string connectionString)
+    {
+        // logic to connect to database
+    }
+
+    public void SetupDatabase(string options)
+    {
+        // logic to setup database
+    }
+
+    public void DisconnectFromDatabase()
+    {
+        // logic to disconnect from database
+    }
+}
+
+public class UserManager
+{
+    public void CreateUser()
+    {
+        // logic to create user
+    }
+
+    public void CreateRole()
+    {
+        // logic to create role
+    }
+    
+    public void AssignRoleToUser()
+    {
+        // logic to assign role to user
+    }
+}
